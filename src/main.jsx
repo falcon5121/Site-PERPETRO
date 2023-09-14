9090327a4f260a93656e456fb9b5c07de21c0982
Emanuel da Silva Gonçalves <fxemanuel@gmail.com>
2023-09-13 23:18:16 -0300

    testando build

--- src/main.jsx
+++ src/main.jsx
@@ -24,45 +24,45 @@ import Digitais from "./Pages/Laboratorio/Digitais/Digitais";
 import Painel from "./Pages/Painel de Controle";
 import Postagens from "./Pages/Laboratorio/Publicacoes/Posts";
 
-// import { Provider } from "react-redux";
-// import store from "./store";
+import { Provider } from "react-redux";
+import store from "./store";
 // import { fetchLogin, login } from "./Redux/variavel";
 
 // store.dispatch(fetchLogin());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <Global />
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="*" element={<Error />} />
+    <Provider store={store}>
+      <Global />
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="*" element={<Error />} />
 
-        <Route path="/pesquisa/pibic" element={<Pibic />} />
-        <Route path="/pesquisa/projetos_coletivos" element={<Pro_Cole />} />
-        <Route path="/pesquisa/projetos_individuais" element={<Pro_Ind />} />
+          <Route path="/pesquisa/pibic" element={<Pibic />} />
+          <Route path="/pesquisa/projetos_coletivos" element={<Pro_Cole />} />
+          <Route path="/pesquisa/projetos_individuais" element={<Pro_Ind />} />
 
-        <Route path="/ensino_e_extensao/cursos" element={<Cursos />} />
-        <Route path="/ensino_e_extensao/oficinas" element={<Oficinas />} />
-        <Route path="/ensino_e_extensao/probex" element={<Probex />} />
-        <Route path="/ensino_e_extensao/prolicen" element={<Prolicen />} />
+          <Route path="/ensino_e_extensao/cursos" element={<Cursos />} />
+          <Route path="/ensino_e_extensao/oficinas" element={<Oficinas />} />
+          <Route path="/ensino_e_extensao/probex" element={<Probex />} />
+          <Route path="/ensino_e_extensao/prolicen" element={<Prolicen />} />
 
-        <Route path="/laboratorio/agenda" element={<Agenda />} />
-        <Route path="/laboratorio/apresentacao" element={<Apresentação />} />
-        <Route path="/laboratorio/parcerias" element={<Parcerias />} />
-        <Route path="/laboratorio/publicacoes" element={<Publicacoes />} />
-        <Route path="/laboratorio/quem_somos" element={<Quem_somos />} />
-        <Route path="/laboratorio/recursos_digitais" element={<Digitais />} />
+          <Route path="/laboratorio/agenda" element={<Agenda />} />
+          <Route path="/laboratorio/apresentacao" element={<Apresentação />} />
+          <Route path="/laboratorio/parcerias" element={<Parcerias />} />
+          <Route path="/laboratorio/publicacoes" element={<Publicacoes />} />
+          <Route path="/laboratorio/quem_somos" element={<Quem_somos />} />
+          <Route path="/laboratorio/recursos_digitais" element={<Digitais />} />
 
-        <Route path="/Painel" element={<Painel />} />
-        {/* <Route path="/editor" element={<Editor />} /> */}
-        <Route
-          path="/laboratorio/publicacoes/postagens/:handle"
-          element={<Postagens />}
-        />
-      </Routes>
-    </Router>
-    {/* </Provider> */}
+          <Route path="/Painel" element={<Painel />} />
+          {/* <Route path="/editor" element={<Editor />} /> */}
+          <Route
+            path="/laboratorio/publicacoes/postagens/:handle"
+            element={<Postagens />}
+          />
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
